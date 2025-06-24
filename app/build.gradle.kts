d23903bb172c1cef503afc00c35b2a642bfd9553
siuhangw <wshiahb@gmail.com>
2025-06-24 17:40:07 +0800

    rollback again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     signingConfigs {
-        getByName("release") {
+        getByName("debug") {
             storeFile = file("RELEASE_STORE_FILE")
             storePassword = "RELEASE_STORE_PASSWORD"
             keyAlias = "RELEASE_KEY_ALIAS"
@@ -28,7 +28,6 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
